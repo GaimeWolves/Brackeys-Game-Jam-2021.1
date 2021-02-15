97451e42046e90baf075ca3828a99fc83f819edc
Nick Djerfi <gamewolvesofficial@gmail.com>
2021-02-15 13:34:15 +0100

    Setup project

---
+++ desktop/src/com/gamewolves/bgj2021/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamewolves.bgj2021.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamewolves.bgj2021.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
